77364749486baf2526ab816c7b8a5f420d3a9df7
salah127 <salah1999jari@gmail.com>
2025-03-10 23:51:44 +0100

    api front undo changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,20 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000"; // Modifier selon le backend
 
-export const fetchTweets = async (token) => {
-  const response = await axios.get(`${API_URL}/tweets`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+export const fetchTweets = async () => {
+  const response = await axios.get(`${API_URL}/tweets`);
   return response.data;
 };
 
-export const postTweet = async (content, token) => {
-  const response = await axios.post(`${API_URL}/tweets`, { content }, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+export const postTweet = async (content) => {
+  const response = await axios.post(`${API_URL}/tweets`, { content });
   return response.data;
 };
